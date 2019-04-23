e53145ab15fdc2cc7ec73db2ca4b33bdedf383ae
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-04-23 16:27:27 -0300

    Criados articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,23 @@
 class ArticlesController < ApplicationController
-    def new
+    def index
+        @article = Article.all
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(article_params())
+        @article.save
+        redirect_to @article
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   resources :articles
   root 'welcome#index'
 
