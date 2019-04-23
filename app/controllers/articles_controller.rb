f5eb022ddd2f2fc80a13ab27454bd03553913393
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-04-23 16:45:11 -0300

    Funcionalidade de edição feita

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,16 +5,28 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
         @article = Article.new(article_params())
-        @article.save
-        redirect_to @article
+        if (@article.save)
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
     def show
         @article = Article.find(params[:id])
     end
-    
-
+    def update
+        @article = Article.find(params[:id])
+        if(@article.update(article_params()))
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
     private
         def article_params
             params.require(:article).permit(:title, :text)
