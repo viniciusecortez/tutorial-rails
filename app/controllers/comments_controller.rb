b9b953a555c6364b558582b34dff2f885adfcb52
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-04-23 18:14:11 -0300

    Inicializando comentários

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path
+    end 
+    private
+    def comment_params
+        params.require(:comment).permit(:commenter, :body)
+    end        
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#index'
 
 end
+ 
+<h2>Add a comment:</h2>
+<%= form_with(model: [ @article, @article.comments.build ], local: true) do |form| %>
+  <p>
+    <%= form.label :commenter %><br>
+    <%= form.text_field :commenter %>
+  </p>
+  <p>
+    <%= form.label :body %><br>
+    <%= form.text_area :body %>
+  </p>
+  <p>
+    <%= form.submit %>
+  </p>
+<% end %>
+ 
+<%= link_to 'Edit', edit_article_path(@article) %> |
+<%= link_to 'Back', articles_path %>
