fac1817741998d0e4d1d70279c27c24ffd02aba1
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-04-29 16:45:43 -0300

    Criado método destroy na classe comments_controler

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,13 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
-    end 
+    end
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+      end 
     private
     def comment_params
         params.require(:comment).permit(:commenter, :body)
