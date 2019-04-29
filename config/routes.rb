57802e0ff4d65b0735521f16803398349c34e3c7
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-04-29 16:18:37 -0300

    Criado vinculo entre comentários e artigo

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,3 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
 end
- 
-<h2>Add a comment:</h2>
-<%= form_with(model: [ @article, @article.comments.build ], local: true) do |form| %>
-  <p>
-    <%= form.label :commenter %><br>
-    <%= form.text_field :commenter %>
-  </p>
-  <p>
-    <%= form.label :body %><br>
-    <%= form.text_area :body %>
-  </p>
-  <p>
-    <%= form.submit %>
-  </p>
-<% end %>
- 
-<%= link_to 'Edit', edit_article_path(@article) %> |
-<%= link_to 'Back', articles_path %>
