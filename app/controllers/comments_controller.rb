eac6ca915b282d1cbbedb69361635796f06d444b
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-04-29 16:19:53 -0300

    Corrigido comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
-        redirect_to article_path
+        redirect_to article_path(@article)
     end 
     private
     def comment_params
